7bb223818083105c07b8c446194dd89c0e5d5d63
treysont1 <treysonthelusma1@gmail.com>
2026-03-07 18:19:26 -0500

    Fix Study Session model

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True)
 
 class StudySession(db.Model):
-    id = db.column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     subject = db.Column(db.String(50), nullable=False)
     location = db.Column(db.String(100), nullable=False)
